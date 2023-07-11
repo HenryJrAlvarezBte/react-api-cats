12c912901dadb656c6e381bb387b3057ff279731
Henry Junior Alvarez Bustamante <86837303+HenryJrAlvarezBte@users.noreply.github.com>
2023-07-11 16:04:45 -0300

    finalizando proyecto api - frases de gatos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showImage, setShowImage] = useState(false);
+
+  const handleClick = async () => {
+    const input = document.getElementById("name");
+    if (input.value !== '') {
+      const cat = input.value;
+      const image = document.getElementById("image");
+      image.src = `https://cataas.com/cat/says/${cat}`;
+      setShowImage(true);
+    } else {
+      setShowImage(false);
+    }
+  };
+
+  async function fetchCatPhrase() {
+    try {
+      const response = await fetch('https://cataas.com/c');
+      const data = await response.json();
+      const catPhrase = data.phrase;
+      document.getElementById("name").value = catPhrase;
+    } catch (error) {
+      console.error('Error al obtener la frase del gato:', error);
+    }
+  }
+
+  fetchCatPhrase();
 
   return (
     <>
+      <h1><u>App Gatos con Frase</u></h1>
+
+      {showImage ? (
+        <img src="./public/cat-say.png" alt="" id="image" />
+      ) : (
+        <img src="./public/cat-say.png" alt="server-api-caido" id="image" />
+      )}
+
+      <button type="button" id="button1" onClick={handleClick}>Gato dice:</button>
+      <input type="text" placeholder="Escribe tu frase aquí y aprieta Gato dice." id="name" required />
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,28 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './index.css';
 
-const button = document.getElementById("button1");
-
-button.addEventListener("click", async function() {
-  if (document.getElementById("name")) {    
-    const cat = document.getElementById("name").value;
-    const image = document.getElementById("image");
-    image.src = `https://cataas.com/cat/says/${cat}`;
-  }
-});
-
-async function fetchCatPhrase() {
-  try {
-    const response = await fetch('https://cataas.com/c');
-    const data = await response.json();
-    const catPhrase = data.phrase;
-    document.getElementById("name").value = catPhrase;
-  } catch (error) {
-    console.error('Error al obtener la frase del gato:', error);
-  }
-}
-
-fetchCatPhrase();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
